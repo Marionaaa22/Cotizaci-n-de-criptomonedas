7d066be69e3eccac6779bacb259c134c4e7ea6d8
Marionaaa22 <mariona.claros@mataro.epiaedu.cat>
2024-10-16 20:07:27 +0200

    listeners fets

--- app/src/main/java/com/mariona/cotitzacio_criptomonedes/MainActivity.kt
+++ app/src/main/java/com/mariona/cotitzacio_criptomonedes/MainActivity.kt
@@ -8,134 +8,144 @@ import android.widget.EditText
 import android.widget.TextView
 import com.google.android.material.dialog.MaterialAlertDialogBuilder
 import com.google.android.material.snackbar.Snackbar
-import java.lang.Exception
 import java.text.DecimalFormat
+import kotlin.Exception
 
 class MainActivity : AppCompatActivity() {
 
+    //variables que encara no s'inicialitzen
     lateinit var txtInput: TextView
     lateinit var txtOutput: TextView
     lateinit var formatoDecimal: DecimalFormat
 
-    private var btnBitcoin: Button? = null
-    private var btnEtherum: Button? = null
-    private var btnTether: Button? = null
-    private var btnXRP: Button? = null
+    //variables
+    private var numeroActual: String = "" //suma de numeros
+    private var cotitzacio: Double = 0.0 //variable per la cotitzacio
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        txtInput = findViewById(R.id.txtInput)
-        txtOutput = findViewById(R.id.txtOutput)
-        formatoDecimal = DecimalFormat("#,##")
+        try {
+            //busquem id de les variables del text
+            txtInput = findViewById(R.id.txtInput)
+            txtOutput = findViewById(R.id.txtOutput)
+            formatoDecimal = DecimalFormat("#,##00")
 
-        btnBitcoin = findViewById(R.id.btnBitcoin)
-        btnEtherum = findViewById(R.id.btnEtherum)
-        btnTether = findViewById(R.id.btnTether)
-        btnXRP = findViewById(R.id.btnXRP)
+            //inicialitzem variables de les criptomonedes
+            val btnBitcoin: Button = findViewById(R.id.btnBitcoin)
+            val btnEtherum: Button = findViewById(R.id.btnEtherum)
+            val btnTether: Button = findViewById(R.id.btnTether)
+            val btnXRP: Button = findViewById(R.id.btnXRP)
 
-        //declarar variables
-        var diners: String = ""
-        var btn0: Button
-        var btn1: Button
-        var btn2: Button
-        var btn3: Button
-        var btn4: Button
-        var btn5: Button
-        var btn6: Button
-        var btn7: Button
-        var btn8: Button
-        var btn9: Button
-
-        var btnCE: Button
-        var btnBorrar: Button
-        var btnComa: Button
-
-        //find id
-        btnBorrar = findViewById(R.id.btnBorrar)
-        btnCE = findViewById(R.id.btnCE)
-        btnComa = findViewById(R.id.btnComa)
-
-        btn0= findViewById(R.id.btn0)
-        btn1= findViewById(R.id.btn1)
-        btn2= findViewById(R.id.btn2)
-        btn3= findViewById(R.id.btn3)
-        btn4= findViewById(R.id.btn4)
-        btn5= findViewById(R.id.btn5)
-        btn6= findViewById(R.id.btn6)
-        btn7= findViewById(R.id.btn7)
-        btn8= findViewById(R.id.btn8)
-        btn9= findViewById(R.id.btn9)
-
-        //fer setOnClickListener
-        btn0.setOnClickListener{
-            txtInput.setText(txtInput.text.toString()+"0")
-            txtOutput.setText("")
-        }
-        btn1.setOnClickListener{
-            txtInput.setText(txtInput.text.toString()+"1")
-            txtOutput.setText("")
-        }
-        btn2.setOnClickListener{
-            txtInput.setText(txtInput.text.toString()+"2")
-            txtOutput.setText("")
-        }
-        btn3.setOnClickListener{
-            txtInput.setText(txtInput.text.toString()+"3")
-            txtOutput.setText("")
-        }
-        btn4.setOnClickListener{
-            txtInput.setText(txtInput.text.toString()+"4")
-            txtOutput.setText("")
-        }
-        btn5.setOnClickListener{
-            txtInput.setText(txtInput.text.toString()+"5")
-            txtOutput.setText("")
-        }
-        btn6.setOnClickListener{
-            txtInput.setText(txtInput.text.toString()+"6")
-            txtOutput.setText("")
-        }
-        btn7.setOnClickListener{
-            txtInput.setText(txtInput.text.toString()+"7")
-            txtOutput.setText("")
-        }
-        btn8.setOnClickListener{
-            txtInput.setText(txtInput.text.toString()+"8")
-            txtOutput.setText("")
-        }
-        btn9.setOnClickListener{
-            txtInput.setText(txtInput.text.toString()+"9")
-            txtOutput.setText("")
-        }
-
-        btnCE.setOnClickListener{
-            txtInput.setText("")
-            txtOutput.setText("")
-        }
-
-        btnBorrar.setOnClickListener{
-            val numeroActual = txtInput.text.toString()
-            if (numeroActual.isNotEmpty()) {
-                val numeroAnterior = numeroActual.last()
-                val textEditat = numeroActual.dropLast(1)
-                txtInput.setText(textEditat)
-                val digitBorrat = numeroAnterior.toString().toInt()
-                diners = (diners.toDouble() - digitBorrat).toString()
+            //fem un set listener i les dirigim cap a la funcio del dialog
+            //per així que posi elvalor
+            btnBitcoin.setOnClickListener {
+                dialogSelecioCripto()
             }
-            else{
-                txtInput.setText("0")
-                diners = ""
+            btnEtherum.setOnClickListener {
+                dialogSelecioCripto()
             }
-            txtOutput.setText("")
-        }
-        btnComa.setOnClickListener{
-            txtInput.setText(txtInput.text.toString()+",")
-            txtOutput.setText("")
+            btnTether.setOnClickListener {
+                dialogSelecioCripto()
+            }
+            btnXRP.setOnClickListener {
+                dialogSelecioCripto()
+            }
+        }catch (e: Exception){
+            supportActionBar?.title = "S'ha produit un error"
         }
     }
 
+    //funcio per cuan apreta algun numero
+    fun apretarNumero(view: View){
+
+        try {
+            // Variable para utilizar uno de los números como botón
+            val button = view as Button
+
+            // Variable para que almacene los números seleccionados
+            val numero = button.text.toString()
+
+            // Verificar si ya hay una coma en el número actual
+            if (numeroActual.contains(",")) {
+                // Verificar si ya tiene dos decimales
+                if (numeroActual.substringAfter(",").length < 2) {
+                    numeroActual += numero
+                    txtInput.text = numeroActual
+                    calcularCotitzacio()
+
+                } else {
+                    mostraError("No pots més de 2 decimals.")
+                }
+            } else {
+                numeroActual += numero
+                txtInput.text = numeroActual
+                calcularCotitzacio()
+            }
+        } catch (e: Exception) {
+            supportActionBar?.title = "S'ha produit un error"
+        }
+    }
+
+    //funció per si borra només un numero
+    fun borrarNumero(view: View){
+
+        if (numeroActual.isNotEmpty()) {
+            numeroActual = numeroActual.dropLast(1)
+            txtInput.text = numeroActual
+            calcularCotitzacio()
+        }
+    }
+
+    //funcio per borrar tot CE
+    fun borrarTot(view: View){
+
+        try {
+            numeroActual = ""
+            txtInput.text = ""
+            txtOutput.text = ""
+            calcularCotitzacio()
+        }catch (e: Exception){
+            mostraError("Error al añadir la coma: ${e.message}")
+        }
+    }
+
+    //funció de la coma
+    fun Coma(view: View){
+
+        try {
+            if (!numeroActual.contains(",")) {
+                numeroActual += ","
+                txtInput.text = numeroActual
+            } else {
+                mostraError("No puedes tener más de una coma.")
+            }
+        } catch (e: Exception) {
+            mostraError("Error al añadir la coma: ${e.message}")
+        }
+    }
+
+    //funció per calcular la cotitzacio
+    fun calcularCotitzacio(){
+        var resultat: Double = 0.0
+
+        try {
+            if (cotitzacio != 0.0 && numeroActual.isNotEmpty()) {
+                val diners = numeroActual.replace(",", ".").toDouble()
+                resultat = diners * cotitzacio
+                txtOutput.text = formatoDecimal.format(resultat)
+            } else {
+                txtOutput.text = "0"
+            }
+        } catch (e: NumberFormatException) {
+            mostraError("Error al calcular la cotización: ${e.message}")
+        } catch (e: Exception) {
+            mostraError("Error inesperado al calcular: ${e.message}")
+        }
+    }
+
+    //funcio del dialog per mostar el valor de la cripto
     fun dialogSelecioCripto(){
         val edtDada: EditText = EditText(this)
         MaterialAlertDialogBuilder(this)
@@ -146,15 +156,23 @@ class MainActivity : AppCompatActivity() {
             .setNegativeButton("Cancelar",null)
             .setPositiveButton("Aceptar"){ dialog, which ->
                 val inputCotitzacio = edtDada.text.toString()
-                if(inputCotitzacio.isNotEmpty()){
+                if(inputCotitzacio.isNotEmpty() && numeroActual.isNotEmpty()){
                     try{
-                        val cotitzacio = inputCotitzacio.toDouble()
-
-
+                        cotitzacio = inputCotitzacio.toDouble()
                     }catch (e: Exception){
-                        (this as MainActivity).supportActionBar?.setTitle("S'ha produit un error")
+                        mostraError("Error inesperado al calcular: ${e.message}")
                     }
+                }else{
+
+                    mostraError("Possa un numerovàlid")
                 }
             }
+            .show()
+    }
+
+    //funcio del snacbar dels errors
+    fun mostraError(missatge: String){
+        Snackbar.make(findViewById(android.R.id.content),
+            missatge, Snackbar.LENGTH_SHORT).show()
     }
 }
